c73804b3be65bc0284a0e2595a5ae475c40a218d
M.khalid <hafizmujadid00@gmail.com>
2023-12-04 12:46:31 +0100

    improve code

--- avro_producer.py
+++ avro_producer.py
@@ -27,62 +27,34 @@ schema_registry_client = SchemaRegistryClient(schema_registry_conf)
 valuejsonFormatSchema = open("./schema/key_schema.avsc", "r").read()
 keyjsonFormatSchema = open("./schema/value_schema.avsc", "r").read()
 
-class User(object):
+class User:
     def __init__(self, name, age, favorite_color, favorite_number):
-        self.name = name
-        self.favorite_color = favorite_color
-        self.favorite_number = favorite_number
-        self.age = age
+        self.name, self.age, self.favorite_color, self.favorite_number = name, age, favorite_color, favorite_number
 
-
-class Key(object):
+class Key:
     def __init__(self, id):
         self.id = id
 
-
 def user_to_dict(user, ctx):
-    return dict(
-        name=user.name,
-        age=user.age,
-        favorite_color=user.favorite_color,
-        favorite_number=user.favorite_number
-    )
+    return dict(name=user.name, age=user.age, favorite_color=user.favorite_color, favorite_number=user.favorite_number)
 
-
-# Converting key object to key for AvroSerializer function
 def key_to_dict(key, ctx):
     return dict(id=key.id)
 
-
-avro_serializer_value = AvroSerializer(
-    schema_registry_client, value_str, user_to_dict
-)
-avro_serializer_key = AvroSerializer(
-    schema_registry_client, key_str, key_to_dict
-)
-
+avro_serializer_value = AvroSerializer(schema_registry_client, valuejsonFormatSchema, user_to_dict)
+avro_serializer_key = AvroSerializer(schema_registry_client, keyjsonFormatSchema, key_to_dict)
 string_serializer = StringSerializer("utf_8")
 
 producer = Producer(conf)
 
 for i in range(1, 100):
-    key = Key(id=i)
-    payload = User(
-        name=fake.name(),
-        age=fake.random.randint(15, 85),
-        favorite_color=fake.random.choice(['red', 'green', 'blue', 'black', 'brown', 'gray', 'yellow', 'white']),
-        favorite_number=fake.random.randint(10, 100))
+    key, payload = Key(id=i), User(fake.name(), fake.random.randint(15, 85), fake.random.choice(['red', 'green', 'blue', 'black', 'brown', 'gray', 'yellow', 'white']), fake.random.randint(10, 100))
 
     topic = "spark_avro"
     producer.produce(
         topic=topic,
-        key=avro_serializer_key(
-            key, SerializationContext(topic, MessageField.KEY)
-        ),
-        value=avro_serializer_value(
-            payload,
-            SerializationContext(topic, MessageField.VALUE),
-        ),
+        key=avro_serializer_key(key, SerializationContext(topic, MessageField.KEY)),
+        value=avro_serializer_value(payload, SerializationContext(topic, MessageField.VALUE)),
     )
 
-    producer.flush()
+producer.flush()
